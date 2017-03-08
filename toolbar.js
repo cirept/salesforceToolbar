69c934b99283ffc481be0b5e4a208551d7678a91
cirept <cirept612@gmail.com>
2017-03-08 15:35:25 -0800

    continue work with breaking down the code

--- toolbar.js
+++ toolbar.js
@@ -34,9 +34,24 @@ jQuery(window).bind("load", function () {
             }),
             $toggleOn = jQuery('<div>').attr({
                 id: 'toggleOn'
+            }).css({
+                "float": "left",
+                width: "70px",
+                margin: "0",
+                padding: "0 0 0 4px",
+                cursor: "pointer"
             }),
             $toggleOff = jQuery('<div>').attr({
-                id: 'toggleOOff'
+                id: 'toggleOff'
+            }).css({
+                "float": "left",
+                width: "70px",
+                margin: "0",
+                padding: "0 0 0 8px",
+                display: "none",
+                cursor: "pointer",
+                "border-right": "2px #aaa solid",
+                height: "16px"
             }),
             caseNo = jQuery("#cas2_ileinner")[0].innerHTML,
             caseNumber = caseNo.substring(0, 8),
@@ -200,6 +215,17 @@ jQuery(window).bind("load", function () {
             }),
             $resultBox = jQuery('<div>').attr({
                 id: 'resultBox'
+            }).css({
+                display: "none",
+                "float": "right",
+                color: "red",
+                "background-color": "white",
+                padding: "0 15px",
+                margin: "2px 0 0 40px",
+                "-moz-border-radius": "8px",
+                "border-radius": "8px",
+                border: "2px #aaa solid",
+                cursor: "text"
             }),
             $caseNo = jQuery('div').attr({
                 id: 'caseNumber',
@@ -209,10 +235,24 @@ jQuery(window).bind("load", function () {
             $idCombo = jQuery('<div>').attr({
                 title: 'Copy WebID and Case Number',
                 id: 'idCombo'
+            }).css({
+                "float": "right",
+                padding: "3px 5px 0",
+                cursor: "pointer",
+                "border-right": "2px #aaa solid",
+                height: "15px",
+                "padding-top": "0px"
             }).text('+'),
             $copyWebID = jQuery('<div>').attr({
                 id: 'copyWebid',
                 title: 'Copy WebID'
+            }).css({
+                "float": "right",
+                padding: "3px 15px 0",
+                cursor: "pointer",
+                "border-right": "2px #aaa solid",
+                height: "15px",
+                "padding-top": "0px"
             }).text(webID),
             $liveSite = jQuery('<a>').attr({
                 target: '_new'
@@ -228,6 +268,8 @@ jQuery(window).bind("load", function () {
             $folderImage = jQuery('<i>').attr({
                 class: 'fa fa-folder-open',
                 title: 'Project Folder Location'
+            }).css({
+                "margin-top": "-3px"
             }),
             $commentCase = jQuery('<a>').attr({
                 target: '_parent',
@@ -294,128 +336,199 @@ jQuery(window).bind("load", function () {
 
         $uiBox.append('<div id="follow" class="funcButtons" style="padding-left:12px; padding-top:5px; float:left; color:blue; font-size:11px;"> <span style="color:black;">Created By:&nbsp;&nbsp;&nbsp;</span><span id="creator" title="Copy Name">' + createdBy + "</span></div>");
 
+        // functions
+        // ----------------------------------------
+        var funcButts = jQuery(".funcButtons").css({
+            display: "none",
+            "float": "right",
+            padding: "3px 15px 0",
+            cursor: "pointer",
+            "border-right": "2px #aaa solid",
+            height: "15px",
+            "padding-top": "0px"
+        });
+
+        $toggleOn.click(function () {
+            $uiBox.animate({
+                width: "75%"
+            }, "slow");
+            $toggleOn.toggle();
+            $toggleOff.toggle();
+            funcButts.toggle();
+        });
+
+        $toggleOff.click(function () {
+            $uiBox.animate({
+                width: "70px"
+            }, "slow");
+            funcButts.css({
+                background: "inherit",
+                "text-shadow": "#aaa 0px 0px 0px"
+            });
+            $toggleOn.toggle();
+            $toggleOff.toggle();
+            funcButts.toggle();
+            $resultBox.hide();
+            $resultBox.text("");
+        });
+
+        $idCombo.click(function () {
+            window.prompt("Copy to clipboard: Ctrl+C, Enter", webID + " " + caseNumber);
+        });
+
+        $caseNo.click(function () {
+            window.prompt("Copy to clipboard: Ctrl+C, Enter", caseNumber);
+        });
+
+        $copyWebID.click(function () {
+            window.prompt("Copy to clipboard: Ctrl+C, Enter", webID);
+        });
+
+        jQuery("#creator").click(function () {
+            window.prompt("Copy to clipboard: Ctrl+C, Enter", createdBy);
+        });
+
+        $copyFolderPath.click(function () {
+            window.prompt("Copy to clipboard: Ctrl+C, Enter", folderPath);
+        });
+
+        // end functions
+        // ----------------------------------------
+
         //        var T = (jQuery("#emailBugTeam"), jQuery("#webID"), jQuery("#assignButton")),
-        var $bugTeam = jQuery("#emailBugTeam"),
-            $webID = jQuery("#webID"),
-            $addignButton = jQuery("#assignButton"),
-            //            E = (jQuery("#jumpLinkDown"), jQuery("#jumpLinkUp"), jQuery("#copyFolderPath")),
-            $jumpLinkDown = jQuery("#jumpLinkDown"),
-            $jumpLinkUp = jQuery("#jumpLinkUp"),
-            $folderPath = jQuery("#copyFolderPath"),
-            Q = jQuery("#copyFolderPath > img"),
-            N = jQuery(".funcButtons"),
-            H = jQuery("#resultBox"),
-            //            M = jQuery("#toggleOn"),
-            //            O = jQuery("#toggleOff"),
-            //            U = jQuery("#idCombo"),
-            $caseNoWebIDCombo = jQuery("#idCombo");
+        //        var $bugTeam = jQuery("#emailBugTeam"),
+        //            $webID = jQuery("#webID"),
+        //            $addignButton = jQuery("#assignButton"),
+        //            E = (jQuery("#jumpLinkDown"), jQuery("#jumpLinkUp"), jQuery("#copyFolderPath")),
+        //            $jumpLinkDown = jQuery("#jumpLinkDown"),
+        //            $jumpLinkUp = jQuery("#jumpLinkUp"),
+        //            $folderPath = jQuery("#copyFolderPath"),
+        //            Q = jQuery("#copyFolderPath > img"),
+        //            N = jQuery(".funcButtons"),
+        //            H = jQuery("#resultBox"),
+        //            M = jQuery("#toggleOn"),
+        //            O = jQuery("#toggleOff"),
+        //            U = jQuery("#idCombo"),
+        //            $caseNoWebIDCombo = jQuery("#idCombo");
         //            I = jQuery("#copyWebid");
         //            I = jQuery("#copyWebid");
 
-        if ($caseNoWebIDCombo.css({
-                "float": "right",
-                padding: "3px 5px 0",
-                cursor: "pointer",
-                "border-right": "2px #aaa solid",
-                height: "15px",
-                "padding-top": "0px"
-            }), $copyWebID.css({
-                "float": "right",
-                padding: "3px 15px 0",
-                cursor: "pointer",
-                "border-right": "2px #aaa solid",
-                height: "15px",
-                "padding-top": "0px"
-            }), N.css({
-                display: "none",
-                "float": "right",
-                padding: "3px 15px 0",
-                cursor: "pointer",
-                "border-right": "2px #aaa solid",
-                height: "15px",
-                "padding-top": "0px"
-            }), H.css({
-                display: "none",
-                "float": "right",
-                color: "red",
-                "background-color": "white",
-                padding: "0 15px",
-                margin: "2px 0 0 40px",
-                "-moz-border-radius": "8px",
-                "border-radius": "8px",
-                border: "2px #aaa solid",
-                cursor: "text"
-            }), $toggleOn.css({
-                "float": "left",
-                width: "70px",
-                margin: "0",
-                padding: "0 0 0 4px",
-                cursor: "pointer"
-            }), $toggleOff.css({
-                "float": "left",
-                width: "70px",
-                margin: "0",
-                padding: "0 0 0 8px",
-                display: "none",
-                cursor: "pointer",
-                "border-right": "2px #aaa solid",
-                height: "16px"
-            }), Q.css({
-                "margin-top": "-3px"
-            }), $toggleOn.click(function () {
-                $uiBox.animate({
-                    width: "75%"
-                }, "slow"), $toggleOn.toggle(), $toggleOff.toggle(), N.toggle();
-            }), $toggleOff.click(function () {
-                $uiBox.animate({
-                        width: "70px"
-                    }, "slow"),
-                    jQuery(".funcButtons").css({
-                        background: "inherit",
-                        "text-shadow": "#aaa 0px 0px 0px"
-                    }), $toggleOn.toggle(),
-                    $toggleOff.toggle(),
-                    N.toggle(),
-                    H.hide(),
-                    H.text("");
-            }),
-            jQuery("#idCombo").click(function () {
-                window.prompt("Copy to clipboard: Ctrl+C, Enter", webID + " " + caseNumber);
-            }),
-            jQuery("#caseNumber").click(function () {
-                window.prompt("Copy to clipboard: Ctrl+C, Enter", caseNumber);
-            }),
-            //            jQuery("#copyWebid").click(function () {
-            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", webID);
-            //            }),
-            $copyWebID.click(function () {
-                window.prompt("Copy to clipboard: Ctrl+C, Enter", webID);
-            }),
-            jQuery("#creator").click(function () {
-                window.prompt("Copy to clipboard: Ctrl+C, Enter", createdBy);
-            }),
-            $folderPath.click(function () {
-                window.prompt("Copy to clipboard: Ctrl+C, Enter", folderPath);
-            }),
-            $bugTeam.click(function () {}), void 0 != jQuery("#cas28_ileinner a").html()) {
-            console.log("child case");
-            var R = '<p style ="color:red; margin-left:5px;"><b>CHILD CASE:</b> Look for related cases</p>';
-            jQuery(".ptBody").after(R);
-        }
-        var P = document.getElementById(a + "_RelatedChildCaseList_link").innerHTML,
-            S = P.replace("]</span></span>", ""),
-            D = S.charAt(S.length - 1);
-        if ("0" != D) {
+        //        if (
+        //            //            $caseNoWebIDCombo.css({
+        //            //                "float": "right",
+        //            //                padding: "3px 5px 0",
+        //            //                cursor: "pointer",
+        //            //                "border-right": "2px #aaa solid",
+        //            //                height: "15px",
+        //            //                "padding-top": "0px"
+        //            //            }), //added
+        //
+        //            //            $copyWebID.css({
+        //            //                "float": "right",
+        //            //                padding: "3px 15px 0",
+        //            //                cursor: "pointer",
+        //            //                "border-right": "2px #aaa solid",
+        //            //                height: "15px",
+        //            //                "padding-top": "0px"
+        //            //            }),
+        //            //            N.css({
+        //            //                display: "none",
+        //            //                "float": "right",
+        //            //                padding: "3px 15px 0",
+        //            //                cursor: "pointer",
+        //            //                "border-right": "2px #aaa solid",
+        //            //                height: "15px",
+        //            //                "padding-top": "0px"
+        //            //            }),
+        //            //            H.css({
+        //            //                display: "none",
+        //            //                "float": "right",
+        //            //                color: "red",
+        //            //                "background-color": "white",
+        //            //                padding: "0 15px",
+        //            //                margin: "2px 0 0 40px",
+        //            //                "-moz-border-radius": "8px",
+        //            //                "border-radius": "8px",
+        //            //                border: "2px #aaa solid",
+        //            //                cursor: "text"
+        //            //            }),
+        //            //            $toggleOn.css({
+        //            //                "float": "left",
+        //            //                width: "70px",
+        //            //                margin: "0",
+        //            //                padding: "0 0 0 4px",
+        //            //                cursor: "pointer"
+        //            //            }),
+        //            //            $toggleOff.css({
+        //            //                "float": "left",
+        //            //                width: "70px",
+        //            //                margin: "0",
+        //            //                padding: "0 0 0 8px",
+        //            //                display: "none",
+        //            //                cursor: "pointer",
+        //            //                "border-right": "2px #aaa solid",
+        //            //                height: "16px"
+        //            //            }),
+        //            //            Q.css({
+        //            //                "margin-top": "-3px"
+        //            //            }),
+        //
+        //            //            jQuery("#idCombo").click(function () {
+        //            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", webID + " " + caseNumber);
+        //            //            }),
+        //            //            jQuery("#caseNumber").click(function () {
+        //            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", caseNumber);
+        //            //            }),
+        //            //            jQuery("#copyWebid").click(function () {
+        //            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", webID);
+        //            //            }),
+        //            //            $copyWebID.click(function () {
+        //            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", webID);
+        //            //            }),
+        //            //            jQuery("#creator").click(function () {
+        //            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", createdBy);
+        //            //            }),
+        //            //            $folderPath.click(function () {
+        //            //                window.prompt("Copy to clipboard: Ctrl+C, Enter", folderPath);
+        //            //            }),
+        ////            $bugTeam.click(function () {}), void 0 != jQuery("#cas28_ileinner a").html()
+        //            ) {
+        //            console.log("child case");
+        //        }
+
+        // ----------------------------------------
+        // check for parent / child lanches
+        // ----------------------------------------
+        /*
+                //        var P = document.getElementById(a + "_RelatedChildCaseList_link").innerHTML,
+        //            S = P.replace("]</span></span>", ""),
+        //            D = S.charAt(S.length - 1);
+
+
+        var rawParentLaunch = jQuery('#CF00N40000002hyAq_ileinner').text(),
+            trimParentLaunch = jQuery.trim(rawParentLaunch);
+        var R = '<p style ="color:red; margin-left:5px;"><b>CHILD CASE:</b> Look for related cases</p>';
+        jQuery(".ptBody").after(R);
+
+        if (trimParentLaunch !== '') {
             var W = '<p style ="color:red; margin-left:5px;"><b>PARENT CASE:</b> Look for related cases</p>';
             jQuery(".ptBody").after(W);
         }
+        */
+        // ----------------------------------------
     }
+    // ----------------------------------------
+    // ----------------------------------------
+
     var t = window.location.href,
         a = t.split("/")[3];
+
     t.search("cdk.my.salesforce.com/500?") && (jQuery("document").ready(function () {
+
         var e = jQuery("#userNavLabel")[0].innerHTML,
             t = jQuery("#cas1_ileinner a")[1].innerHTML,
             a = jQuery("#CF00N40000002aU5w_ileinner")[0].innerHTML;
+
         "&nbsp;" == a && jQuery("#CF00N40000002aU5w_ileinner").css("backgroundColor", "#FFF85E"), t == e && jQuery("#CF00N40000002aU5w_ileinner").dblclick(function () {
             jQuery("#CF00N40000002aU5w").val(t);
         }), setTimeout(function () {
@@ -426,17 +539,41 @@ jQuery(window).bind("load", function () {
     // ----------------------------------------
     // ----------------------------------------
 
-    var r = window.location.pathname,
-        a = r.slice(1),
-        o = document.getElementById(a + "_RelatedCommentsList_link").innerHTML,
-        i = o.replace("]</span></span>", ""),
-        n = i.charAt(i.length - 1),
-        d = jQuery("#cas7_ileinner")[0].innerHTML;
-    "0" != n && "Closed" != d && (document.getElementById(a + "_RelatedCommentsList_link").style.backgroundColor = "red");
-    var l = document.getElementById(a + "_RelatedAttachmentList_link").innerHTML,
-        p = l.replace("]</span></span>", ""),
-        c = p.charAt(p.length - 1);
-    "0" != c && (document.getElementById(a + "_RelatedAttachmentList_link").style.backgroundColor = "gold");
+    // ----------------------------------------
+    // highlight things on page
+    // ----------------------------------------
+    //    var r = window.location.pathname,
+    var path = window.location.pathname,
+        caseID = path.slice(1);
+    var $comments = jQuery("#5003300000vntf2_RelatedCommentsList_link");
+    var commentAmount = $comments.find('.count');
+
+    if (commentAmount !== 0) {
+        $comments.css({
+            background: 'red'
+        });
+    }
+
+    // TO DO CREATE finding feature for attachments
+
+    //    var path = window.location.pathname,
+    //        caseID = path.slice(1),
+    //        a = r.slice(1),
+    //        o = document.getElementById(caseID + "_RelatedCommentsList_link").innerHTML,
+    //        o = document.getElementById(a + "_RelatedCommentsList_link").innerHTML,
+    //        i = o.replace("]</span></span>", ""),
+    //        n = i.charAt(i.length - 1),
+    //        d = jQuery("#cas7_ileinner")[0].innerHTML;
+
+    //    "0" != n && "Closed" != d && (document.getElementById(a + "_RelatedCommentsList_link").style.backgroundColor = "red");
+    //    "0" != n && "Closed" != d && (document.getElementById(caseID + "_RelatedCommentsList_link").style.backgroundColor = "red");
+
+    //    var l = document.getElementById(a + "_RelatedAttachmentList_link").innerHTML,
+    //    var l = document.getElementById(caseID + "_RelatedAttachmentList_link").innerHTML,
+    //        p = l.replace("]</span></span>", ""),
+    //        c = p.charAt(p.length - 1);
+    //    "0" != c && (document.getElementById(a + "_RelatedAttachmentList_link").style.backgroundColor = "gold");
+    //    "0" != c && (document.getElementById(caseID + "_RelatedAttachmentList_link").style.backgroundColor = "gold");
 });
 
 
