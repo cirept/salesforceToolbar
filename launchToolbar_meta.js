a14fb31c6e40f9fdf7ac1d6f75bde2d808ebf09f
Eric Tanaka <cirept@users.noreply.github.com>
2017-05-03 15:20:00 -0700

    Update launchToolbar_meta.js

--- launchToolbar_meta.js
+++ launchToolbar_meta.js
@@ -3,12 +3,12 @@
 // @namespace www.cobaltgroup.com
 // @include https://cdk.my.salesforce.com/a1E*
 // @include https://cdk--c.na27.visual.force.com/*
-// @version 3.9
+// @version 4.0
 // @run-at document-end
 // @downloadURL https://raw.githubusercontent.com/cirept/salesforceToolbar/master/launchToolbar_meta.js
 // @require https://use.fontawesome.com/3953f47d82.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
-// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/master/launchToolbar_release.js
+// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/v4.0/launchToolbar_release.js
 // @author cire
 // @grant unsafeWindow
 // @grant GM_setClipboard
