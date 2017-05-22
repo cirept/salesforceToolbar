de0c4b5078c18803b539540c6d03687a8f1bc1c4
Eric Tanaka <cirept@users.noreply.github.com>
2017-05-22 11:09:26 -0700

    Fixed URL issue
    SalesForce Summary URL fix.
    https://cdk--c.na27.visual.force.com/ --> https://cdk--c.na57.visual.force.com/
    URL Update to latest version
    /v4.2/ --> /v4.3/

--- launchToolbar_meta.js
+++ launchToolbar_meta.js
@@ -2,15 +2,15 @@
 // @name Salesforce Launch Toolbar
 // @namespace www.cobaltgroup.com
 // @include https://cdk.my.salesforce.com/a1E*
-// @include https://cdk--c.na27.visual.force.com/*
-// @version 4.2
+// @include https://cdk--c.na57.visual.force.com/*
+// @version 4.3
 // @run-at document-end
 // @downloadURL https://raw.githubusercontent.com/cirept/salesforceToolbar/master/launchToolbar_meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
-// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/v4.2/launchToolbar_release.js
+// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/v4.3/launchToolbar_release.js
 // @author cire
 // @grant unsafeWindow
 // @grant GM_setClipboard
