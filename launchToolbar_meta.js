a9027035bfdb7a5457d564827b511276a7e378f9
Eric Tanaka <eric.tanaka>
2017-08-08 09:11:55 -0700

    updated meta tag to 4.5 release

--- launchToolbar_meta.js
+++ launchToolbar_meta.js
@@ -1,16 +1,16 @@
 // ==UserScript==
-// @name Salesforce Launch Toolbar
+// @name Salesforce Launch Toolbar 4.5
 // @namespace www.cobaltgroup.com
 // @include https://cdk.my.salesforce.com/a1E*
 // @include https://cdk--c.na57.visual.force.com/*
-// @version 4.4
+// @version 4.5
 // @run-at document-end
 // @downloadURL https://raw.githubusercontent.com/cirept/salesforceToolbar/master/launchToolbar_meta.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
-// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/v4.4/launchToolbar_release.js
+// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/v4.5/launchToolbar_release.js
 // @author cire
 // @grant unsafeWindow
 // @grant GM_setClipboard
