85ec0978e9df2b4d9bbf0e0a64ee478183c4311a
Jun <chongjk88@hotmail.com>
2017-11-10 06:25:17 -0800

    Changed title and updated @require URL
    Removed the version number from the title as requested, and updated the @require URL to the launchToolbar.js. I'll test out the encrypted version in a later time. ;)

--- launchToolbar_meta.js
+++ launchToolbar_meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name Salesforce Launch Toolbar 4.5
+// @name Salesforce Launch Toolbar
 // @namespace www.cobaltgroup.com
 // @include https://cdk.my.salesforce.com/a1E*
 // @include https://cdk--c.na57.visual.force.com/*
@@ -10,7 +10,7 @@
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
-// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/v4.6/launchToolbar_release.js
+// @require https://raw.githubusercontent.com/cirept/salesforceToolbar/master/launchToolbar.js
 // @author cire
 // @grant unsafeWindow
 // @grant GM_setClipboard
