f57e6242ce70cbf80e0aab50920744699cb617e1
Eric Tanaka <eric.tanaka>
2018-03-23 09:20:07 -0700

    commiting changes

---
+++ assets/js/HOW_TO_UPDATE.js
@@ -0,0 +1,16 @@
+/**
+* UPDATE STEPS
+* 1. Work from 'launchToolbar.js' file, make edits and changes in this file
+* 2. Once changes are complete, Move old 'launchToolbar_release.js' into the archive folder
+* 3. Go to 'https://javascriptobfuscator.herokuapp.com/' to obfuscate the 'launchToolbar.js' file
+* 4. Download the obfucated file and rename to 'launchToolbar_release.js'
+* 5. Move the file into the 'assets/js' folder
+* 6. Update the meta tag to reflect the latest version e.g. 4.5 or 4.6.7
+* 7. Update the git repo
+* 8. Create a new release, for versioning purposes.
+*/
+
+/*
+Update Details
+
+*/
