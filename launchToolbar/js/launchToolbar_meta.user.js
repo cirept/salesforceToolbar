c27b179fe55c69a44e580a5781861e91a9bf2dcb
Jun Kim <chongjk88@hotmail.com>
2018-06-25 08:36:09 -0700

    Update launchToolbar_meta.user.js

--- launchToolbar/js/launchToolbar_meta.user.js
+++ launchToolbar/js/launchToolbar_meta.user.js
@@ -5,12 +5,12 @@
 // @include https://cdk--c.na57.visual.force.com/*
 // @version 4.8.3.2
 // @run-at document-end
-// @downloadURL https://github.com/cirept/salesforceToolbar/raw/develop/launchToolbar/js/launchToolbar_meta.user.js
+// @downloadURL https://github.com/cirept/salesforceToolbar/raw/master/launchToolbar/js/launchToolbar_meta.user.js
 // @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
-// @require https://github.com/cirept/salesforceToolbar/raw/develop/launchToolbar/js/launchToolbar.js
+// @require https://github.com/cirept/salesforceToolbar/raw/master/launchToolbar/js/launchToolbar.js
 // @author cire
 // @grant unsafeWindow
 // @grant GM_setClipboard
