3b4490f7b434e6085a08fe11d29872c7fbe26632
Jun Kim <chongjk88@hotmail.com>
2018-06-25 08:17:17 -0700

    Create launchToolbar_meta_prerelease.user.js

---
+++ launchToolbar/js/launchToolbar_meta_prerelease.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name Salesforce Launch Toolbar (Pre-release)
+// @namespace www.cobaltgroup.com
+// @include https://cdk.my.salesforce.com/a1E*
+// @include https://cdk--c.na57.visual.force.com/*
+// @version 4.8.3.2
+// @run-at document-end
+// @downloadURL https://github.com/cirept/salesforceToolbar/raw/develop/launchToolbar/js/launchToolbar_meta.user.js
+// @require https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js
+// @resource fontAwe https://use.fontawesome.com/3953f47d82.js
+// @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
+// @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
+// @require https://github.com/cirept/salesforceToolbar/raw/develop/launchToolbar/js/launchToolbar.js
+// @author cire
+// @grant unsafeWindow
+// @grant GM_setClipboard
+// @grant GM_openInTab
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant window.close
+// @grant window.focus
+// @grant GM_getResourceURL
+// @grant GM_info
+// @grant GM_listValues
+// @noframes
+// ==/UserScript==
